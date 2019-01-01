5a695517d45a9ac727cc0ffdf80e4fa560d38941
F John De Costa <john@mayerfleur.com>
2019-01-01 13:05:35 +1100

    unignored dist folded

---
+++ dist/app.js
@@ -0,0 +1,76 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId])
+/******/ 			return installedModules[moduleId].exports;
+
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			exports: {},
+/******/ 			id: moduleId,
+/******/ 			loaded: false
+/******/ 		};
+
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+
+/******/ 		// Flag the module as loaded
+/******/ 		module.loaded = true;
+
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+
+
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "/";
+
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(0);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ (function(module, exports, __webpack_require__) {
+
+	module.exports = __webpack_require__(1);
+
+
+/***/ }),
+/* 1 */
+/***/ (function(module, exports) {
+
+	'use strict';
+
+	// JS Goes here - ES6 supported
+	var mySiema = new Siema({
+	  selector: '.gallery-carousel',
+	  duration: 200,
+	  easing: 'ease',
+	  perPage: 1,
+	  startIndex: 0,
+	  draggable: true,
+	  threshold: 20,
+	  loop: true
+	});
+
+	document.querySelector('.prev').addEventListener('click', function () {
+	  return mySiema.prev();
+	});
+	document.querySelector('.next').addEventListener('click', function () {
+	  return mySiema.next();
+	});
+
+/***/ })
+/******/ ]);
